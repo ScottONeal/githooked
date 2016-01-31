731d529bd735e0a16a4bc817547ab9a7184e9f36
Scott O'Neal <scottmod@gmail.com>
2016-01-30 20:33:25 -0500

    created tests for new options argument. Not adding logFile at the moment. #2

--- test/test.js
+++ test/test.js
@@ -15,11 +15,106 @@ describe('githooked server', function() {
      request = supertest(server);
   });
 
+  it('should have property githookedServer.ghRef', function() {
+    assert.equal(server.ghRef, 'test');
+  });
+
+  it('should have property githookedServer.ghAction', function() {
+    assert.equal(server.ghAction, 'exit 1');
+  });
+
+  it('should have property githookedServer.ghOptions', function() {
+    var options = { json: '10mb', logFile: './goothoked.log' };
+    var scopedServer = githooked('test', 'exit 1', options);
+    assert.equal(scopedServer.ghOptions, options);
+  });
+
+  it('defaults action to first argument if only one argument sent', function() {
+    var scopedServer = githooked('exit 1');
+    assert(scopedServer);
+  });
+
+  it('should throw an error when if option argument is not an object', function() {
+    assert.throws(function() {
+      githooked('test', 'exit 1', []);
+    }, Error);
+  });
+
+  it('should accept bodyParser.json middleware options', function(done) {
+    var scopedServer = githooked('test', 'exit 1', { json: { limit: '1kb'}});
+    var buf = new Buffer(1024);
+    buf.fill('.');
+
+    supertest(scopedServer)
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify({ str: buf.toString() }))
+      .expect(413, done);
+  });
+
+  it('should accept a default payload size limit of 1mb', function(done) {
+    var buf = new Buffer(999990);
+    buf.fill('.');
+
+    request
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify({ ref: 'test', str: buf.toString() }))
+      .expect(202, done);
+  });
+
+  it('should add a middleware function if options.middlware = function', function(done) {
+    var middlewarePassed = false;
+
+    var scopedServer = githooked('test', 'exit 1', { middleware:
+       function(req, res, next) {
+         middlewarePassed = true;
+         next();
+       }
+    });
+
+    supertest(scopedServer)
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify({ ref: 'test' }))
+      .end(function(err) {
+        assert.equal(middlewarePassed, true);
+        done(err);
+      });
+  });
+
+  it('should accept an array of middleware functions, if optons.middleware = [function]', function(done) {
+    var middleware1Passed = false,
+        middleware2Passed = false;
+
+    var scopedServer = githooked('test', 'exit 1', { middleware: [
+       function(req, res, next) {
+         middleware1Passed = true;
+         next();
+       },
+       function(req, res, next) {
+         middleware2Passed = true;
+         next();
+       },
+    ]});
+
+    supertest(scopedServer)
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify({ ref: 'test' }))
+      .end(function(err) {
+        assert.equal(middleware1Passed, true);
+        assert.equal(middleware2Passed, true);
+        done(err);
+      });
+
+  });
+
   it('should send a 200 back for a PING event', function(done) {
     request
       .post('/')
       .set('X-GitHub-Event', 'ping')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({zen: 'thisisatest', hook_id: 'thisisatest', hook: 'thisisatest'})
       .expect(200, done);
   });
@@ -27,9 +122,9 @@ describe('githooked server', function() {
   it('should throw an error on invalid payload', function(done) {
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send('asdf')
-      .expect(500, done);
+      .expect(400, done);
   });
 
   it('should emit an error on invalid payload', function(done) {
@@ -40,9 +135,9 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({})
-      .expect(500)
+      .expect(400)
       .end(function(err) {
         assert(err instanceof Error);
       });
@@ -56,7 +151,7 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({ ref: 'test', created: true })
       .expect(202)
       .end(function(err) {
@@ -72,7 +167,7 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({ ref: 'test', deleted: true })
       .expect(202)
       .end(function(err) {
@@ -88,7 +183,7 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({ ref: 'test' })
       .expect(202)
       .end(function(err) {
@@ -103,7 +198,7 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({ ref: 'test' })
       .expect(202)
       .end(function(err) {
@@ -123,7 +218,7 @@ describe('githooked server', function() {
 
     request
       .post('/')
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .send({ ref: 'test' })
       .expect(202)
       .end(function(err) {
